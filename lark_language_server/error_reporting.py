e2bcf812e9d85d04fb88d2ea22dfa823b0b0f201
Robert McGregor <rmcgregor1990@gmail.com>
2020-09-23 23:35:09 +0100

    error reporting working with lark "resumable" branch

--- lark_language_server/error_reporting.py
+++ lark_language_server/error_reporting.py
@@ -1,8 +1,17 @@
-from typing import List
+from typing import List, Union
+import logging
 from lark import UnexpectedToken
 from pygls.types import (Diagnostic, Range, Position)
 
 from .lark_grammar import lark_grammar_parser
+
+
+def user_repr(error: Union[UnexpectedToken]):
+    if isinstance(error, UnexpectedToken):
+        expected = ', '.join(error.accepts or error.expected)
+        return f"Unexpected token {str(error.token)!r}. Expected one of:\n{{{expected}}}"
+    else:
+        return str(error)
 
 
 def get_diagnostics(doctext: str):
@@ -14,11 +23,11 @@ def get_diagnostics(doctext: str):
                 Position(e.line - 1, e.column - 1),
                 Position(e.line - 1, e.column)
             ),
-            "unexpected input"))
+            user_repr(e)))
         return True
 
     try:
         lark_grammar_parser.parse(doctext, on_error=on_error)
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("parser raised exception")
     return diagnostics
